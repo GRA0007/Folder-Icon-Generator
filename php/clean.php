9eeed70804174eccd8a792ee8c681e4dd93f3ad8
Benjamin Grant <benjamin.grant@wesleycollege.net>
2016-11-16 14:10:02 +1100

    Formatting and messages for all occasions

--- php/clean.php
+++ php/clean.php
@@ -1,19 +1,23 @@
 <?php
-  $files = glob("../temp/*");
-  $now   = time();
+$files = glob("../temp/*");
+$now   = time();
 
-  if (count($files) != 0) {
-	  $i = 0;
-	  foreach ($files as $file) {
+if (count($files) != 0) {
+	$i = 0;
+	foreach ($files as $file) {
 		if (is_file($file)) {
-		  if ($now - filemtime($file) >= 60 * 60 * 3) { // 3 hours
-			unlink($file);
-			$i++;
-		  }
+			if ($now - filemtime($file) >= 60 * 60 * 3) { // 3 hours
+				unlink($file);
+				$i++;
+			}
 		}
-	  }
-	  echo "Cleaned " . $i . " files from the temp folder.";
-  } else {
-	  die('Nothing to clean.');
-  }
+	}
+	if ($i > 0) {
+		echo "Cleaned " . $i . " files from the temp folder.";
+	} else {
+		die('Found files, but none over 3 hours.');
+	}
+} else {
+	die('Nothing to clean, folder empty.');
+}
 ?>
